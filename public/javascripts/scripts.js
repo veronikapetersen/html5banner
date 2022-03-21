c34b9619dec6cdb949b2a29468fdf10b3c97f5c6
Veronika Petersen <veronika.petersen@deptagency.com>
2022-03-21 13:00:46 +0100

    span file name overflow

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -102,3 +102,52 @@ document.querySelector("#resetButton").addEventListener("click", deleteFiles);
 async function deleteFiles() {
   const response = await fetch(`/delete-files`)
 }
+
+
+// const dropAreaList = document.querySelectorAll(".asset");
+
+// Array.prototype.forEach.call(dropAreaList, function (dropArea) {
+//   dropArea.addEventListener("drag", function dragOverHandler(ev) {
+//     console.log(dropArea.length);
+//     ev.preventDefault();
+//   })
+//   dropArea.addEventListener("drop", function dropHandler(ev) {
+//     console.log(dropArea.length);
+//     ev.preventDefault();
+//   })
+//   // dropArea.addEventListener("ondrag", dragOverHandler);
+//   // dropArea.addEventListener("ondrop", dropHandler);
+// })
+
+
+// function dragOverHandler(ev) {
+//   console.log('File(s) in drop zone');
+//   // Prevent default behavior (Prevent file from being opened)
+//   ev.preventDefault();
+// }
+
+// //drag and drop files upload
+// function dropHandler(ev) {
+//   console.log('File(s) dropped');
+
+//   // Prevent default behavior (Prevent file from being opened)
+//   ev.preventDefault();
+
+//   if (ev.dataTransfer.items) {
+//     // Use DataTransferItemList interface to access the file(s)
+//     for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+//       // If dropped items aren't files, reject them
+//       if (ev.dataTransfer.items[i].kind === 'file') {
+//         var file = ev.dataTransfer.items[i].getAsFile();
+//         console.log('... file[' + i + '].name = ' + file.name);
+//       }
+//     }
+//   } else {
+//     // Use DataTransfer interface to access the file(s)
+//     for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+//       console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+//     }
+//     updateFileInput(input, fileName, false)
+//   }
+// }
+
