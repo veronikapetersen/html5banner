b4093127ee73478d3eb829ec5b1e021190b718a4
Veronika P <karavannika@gmail.com>
2024-02-15 16:42:15 +0100

    edits

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,13 @@ const glob = require("glob");
 const parse = require('node-html-parser').parse;
 
 
-
-/* GET home page. */
 router.get('/', function (req, res, next) {
   const dirpath = path.resolve('./uploads/');
   res.render('index', { title: 'DEPT' });
 });
 
 var multer = require('multer');
-// const { createInflateRaw } = require('zlib');
-let size = 'bum';
+let size = null;
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -38,7 +35,7 @@ router.post('/profile-upload-single/size/:size',
     { name: 'txt_1' },
     { name: 'txt_2' },
     { name: 'cta' }
-  ]), 
+  ]),
   function (req, res, next) {
     fs.copyFile(`templates/tem_${size}.html`, `uploads/${size}/${size}.html`, (err) => {
       console.log('copy done');
@@ -50,7 +47,7 @@ router.post('/profile-upload-single/size/:size',
 router.post("/compress", (req, res) => {
   const zip = new admzip();
   console.log('zip folder size', size);
-  
+
   zip.addLocalFolder(path.resolve(`uploads/${size}`));
   const name = `${size}.zip`;
   zip.writeZip(name);
@@ -60,38 +57,37 @@ router.post("/compress", (req, res) => {
 
 
 router.get("/delete-files", (req, res) => {
-    // const dir = "uploads/**/*.!(html)";
-    const dir = "uploads/**/*.*";
-    const zip = "*.zip";
+  const dir = "uploads/**/*.*";
+  const zip = "*.zip";
 
-    function bum(files) {
-      files.forEach(file => {
-        fs.unlink(file, (err) => {
-          if (err) throw err;
-        });
-      })
-    }
-
-    glob(zip, {}, function (er, files) {
-      bum(files);
+  function bum(files) {
+    files.forEach(file => {
+      fs.unlink(file, (err) => {
+        if (err) throw err;
+      });
     })
-        
-    glob(dir, {}, function (er, files) {
-      bum(files);
-    })
+  }
 
-    return res.send(true)
+  glob(zip, {}, function (er, files) {
+    bum(files);
+  })
+
+  glob(dir, {}, function (er, files) {
+    bum(files);
+  })
+
+  return res.send(true)
 })
 
 
 
 router.get('/animations/:animation', (req, res) => {
   animation = req.params.animation;
-  
+
   //get the animation JS
   const bufferJS = fs.readFileSync(`animations/${animation}.js`);
   const fileContentJS = bufferJS.toString();
-  
+
   // get the animation CSS
   const cssFile = `animations/${animation}.css`;
   let fileContentCss;
@@ -103,53 +99,53 @@ router.get('/animations/:animation', (req, res) => {
     console.log("no css file found");
     fileContentCss = "";
   }
-  
+
   createFile(fileContentJS, fileContentCss);
   return res.send(true)
 })
 
 function createFile(fileContentJS, fileContentCss) {
   // fs.readFile('templates/tem_300x600.html', 'utf8', (err,html)=>{
-    fs.readFile(`templates/tem_${size}.html`, 'utf8', (err,html)=>{
-      if(err){
-        throw err;
-      }
-      
-      console.log(size);
-      const root = parse(html);
-      const script = root.querySelector('#animation');
-      script.innerHTML = fileContentJS;
-      
-      const ctaAnim = root.querySelector("#animationCss");
-      ctaAnim.innerHTML = fileContentCss;
-      const newFile = root.toString();
-      fs.writeFile(`uploads/${size}/${size}.html`, newFile, (err) => {
-        if (err) throw err;
-      })
-    }) 
-  }
+  fs.readFile(`templates/tem_${size}.html`, 'utf8', (err, html) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(size);
+    const root = parse(html);
+    const script = root.querySelector('#animation');
+    script.innerHTML = fileContentJS;
+
+    const ctaAnim = root.querySelector("#animationCss");
+    ctaAnim.innerHTML = fileContentCss;
+    const newFile = root.toString();
+    fs.writeFile(`uploads/${size}/${size}.html`, newFile, (err) => {
+      if (err) throw err;
+    })
+  })
+}
 
 
 
-  router.get("/noanim", (req, res) => {
-    fs.readFile(`uploads/${size}/${size}.html`, 'utf8', (err,html)=>{
-        console.log("success");  
-        const root = parse(html);
-        const script = root.querySelector('#animation');
-        script.innerHTML = '';
-        const ctaAnim = root.querySelector("#animationCss");
-        ctaAnim.innerHTML = '';
-        const newFile = root.toString();
-        
-        if(err){
-          throw err;
-        }
+router.get("/noanim", (req, res) => {
+  fs.readFile(`uploads/${size}/${size}.html`, 'utf8', (err, html) => {
+    console.log("success");
+    const root = parse(html);
+    const script = root.querySelector('#animation');
+    script.innerHTML = '';
+    const ctaAnim = root.querySelector("#animationCss");
+    ctaAnim.innerHTML = '';
+    const newFile = root.toString();
 
-        fs.writeFile(`uploads/${size}/${size}.html`, newFile, (err) => {
-          if (err) throw err;
-        })
-    }) 
-    return res.send(true)
-  }) 
+    if (err) {
+      throw err;
+    }
 
-  module.exports = router;
+    fs.writeFile(`uploads/${size}/${size}.html`, newFile, (err) => {
+      if (err) throw err;
+    })
+  })
+  return res.send(true)
+})
+
+module.exports = router;
